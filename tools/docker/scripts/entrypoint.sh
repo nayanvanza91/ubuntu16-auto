ac4811f9a04a1003df2a521fb14ee0b20649cf29
nayanvanza91 <nayanvanza91@users.noreply.github.com>
2017-06-14 23:05:46 +0530

    Update entrypoint

--- tools/docker/scripts/entrypoint.sh
+++ tools/docker/scripts/entrypoint.sh
@@ -3,9 +3,10 @@ set -e
 
 #create deployment user and set password
 if [ ! -f docker_initialized ]; then
-	useradd -d /var/www/$UNIT/$PROJECT_NAME/ -g www-data -s /bin/bash $USER
-	usermod -p $(echo $PASSWORD | openssl passwd -1 -stdin) $USER
-	touch docker_initialized
+        useradd -s /bin/bash $USER
+        usermod -p $(echo $PASSWORD | openssl passwd -1 -stdin) $USER
+        touch docker_initialized
 fi
 
 exec "$@"
+#exec supervisord -n
